4b1a0cfaad88a1d661c2ebaf3fb2d5574a62c9de
Umar <umar.zie10@gmail.com>
2025-12-15 14:06:24 +0500

    add commit

--- src/main/java/group_tg/Mybot.java
+++ src/main/java/group_tg/Mybot.java
@@ -1,7 +1,9 @@
 package group_tg;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Mybot extends TelegramLongPollingBot {
     MyBotService myBotService = new MyBotService();
@@ -17,12 +19,26 @@ public class Mybot extends TelegramLongPollingBot {
             String firstName = update.getMessage().getChat().getFirstName();
             String lastName = update.getMessage().getChat().getLastName();
 
+
+
+        } else if (update.hasCallbackQuery()) {
+            CallbackQuery callbackQuery = update.getCallbackQuery();
+            String data = callbackQuery.getData();
+            Long chatId = callbackQuery.getMessage().getChatId();
+
+            if (data.equals("UzbektiliId")) {
+                try {
+                    execute(myBotService.menu(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
     }
 
     @Override
     public String getBotUsername() {
-        return "shopping111Bot";
+        return "";
     }
 
 
@@ -31,6 +47,6 @@ public class Mybot extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
 
-        return "7772292896:AAGZhKZ4_4x9JGnxy98AEo8Xcgj9dx4Kh_A";
+        return "";
     }
 }
